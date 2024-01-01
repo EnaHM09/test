ba9a4b1f8b7c783c3848ba2b019d06a3b5cf175f
Ena <you@example.com>
2024-01-01 20:32:20 +0000

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(`Hi`)
+function click(){
+    //console.log(`Thanks`)
+    alert(`hello`)
+}
+let button = document.querySelector(`button`)
+console.log(button)
+button.addEventListener(`click`, click)
