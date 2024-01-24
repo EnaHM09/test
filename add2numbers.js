83f176d4822f42a2585f487599ad93e9634ddd32
Ena <you@example.com>
2024-01-24 17:25:47 +0000

    save

--- add2numbers.js
+++ add2numbers.js
@@ -1,11 +1,17 @@
 console.log(`result`)
 function add2Numbers(x, y) {
-    return x + y
+    return Number(x) + +y
+
 }
 function click(){
-    let input = document.querySelector(`input`)
-    console.log(input.value)
-    
+    let input = document.querySelectorAll(`input`)
+    let el=document.querySelector(`#x`)
+    console.log(el.value)
+    console.log(input)
+    let x = input[0].value
+    let y = input[1].value
+    let answer = add2Numbers(x, y)
+    alert(answer)
 }
 let button = document.querySelector(`button`)
 console.log(button)
